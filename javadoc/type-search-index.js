1fae1d94721145bb395f007fcf861eadeb431185
David Iveković <david.ivekovic@memgraph.io>
2024-02-03 17:51:07 +0100

    cleanup

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Contact"},{"p":"org.example","l":"ContactReader"},{"p":"org.example","l":"ContactsManager"},{"p":"org.example","l":"Main"}];updateSearchResults();
